7f654ec8b14c40838f843b2f932e12b00463ffae
Ahmad Syukron <ahmad05102001@gmail.com>
2023-04-03 10:43:58 +0700

    menambah seluruh file

---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,13 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : Ahmad Syukron</p>
+        <p>Nim : 312110056</p>
+        <p>Kelas : TI.21.A.1</p>
+        <p>Matkul : Pemograman Web 02</p>
+        <p>Email : ahmad@gmail.com</p>
+        <p>No. Telp : 081267891232</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
